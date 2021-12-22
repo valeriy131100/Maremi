5b09fc2978f87454af017320970ef44ead0aace9
Valery Efremov <valeriy131100@gmail.com>
2021-12-22 19:38:46 +0300

    Now num button doesn't make lag

--- main.py
+++ main.py
@@ -278,8 +278,6 @@ async def get_gallery_message(attachment_id, gallery_id):
 async def handle_gallery_button(interaction: discord.MessageInteraction):
     payload = interaction.component.custom_id
     _, command, attachment_id, gallery_id = payload.split()
-    if command == 'num':
-        return
     attachment_id = int(attachment_id)
     gallery_id = int(gallery_id)
     attachments = await db_helpers.get_gallery_images(gallery_id)
