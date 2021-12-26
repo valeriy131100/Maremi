600d24e87ba684b5efe55f6099a7258d72e50748
Valery Efremov <valeriy131100@gmail.com>
2021-12-26 15:57:33 +0300

    Fix vk-bot connect commands

--- bots/vk/vk_to_discord/connect.py
+++ bots/vk/vk_to_discord/connect.py
@@ -6,13 +6,13 @@ from bots.vk.custom_rules import StartsWithRule
 bp = Blueprint('vk_to_discord_connect')
 
 
-@bp.on.chat_message(StartsWithRule('makeonline'))
+@bp.on.chat_message(StartsWithRule('makeonline', return_text=False))
 async def make_online(message: vkbottle.bot.Message):
     bots.temp['chats'][message.chat_id] = True
     await message.answer(f'К чату теперь можно подключиться. Напишите дискорд-боту m.connect {message.chat_id}')
 
 
-@bp.on.chat_message(StartsWithRule('makeoffline'))
+@bp.on.chat_message(StartsWithRule('makeoffline', return_text=False))
 async def make_offline(message: vkbottle.bot.Message):
     bots.temp['chats'][message.chat_id] = False
     await message.answer(f'К чату теперь нельзя подключиться')
