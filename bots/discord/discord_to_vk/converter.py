4983dbff02bae57cb1cb7e8ab894b898d7c806bc
Valery Efremov <valeriy131100@gmail.com>
2022-01-28 14:10:46 +0300

    Refactor discord_to_vk converter

--- bots/discord/discord_to_vk/converter.py
+++ bots/discord/discord_to_vk/converter.py
@@ -5,28 +5,18 @@ import disnake as discord
 from vkbottle import PhotoMessageUploader
 
 import bots
-from bots.discord.utils.emojis import (EMOJI_REGEX, download_emoji,
-                                       download_file)
+from bots.discord.utils.file_workers import (EMOJI_REGEX, download_emoji,
+                                             download_file)
 from bots.vk.utils import get_random_id
 from models import DiscordNickName, MessageToMessage, Server
 
 
 async def get_vk_message(discord_message: discord.Message):
+    message_text = discord_message.content
+
     nickname = (await DiscordNickName.get_or_none(
         discord_id=discord_message.author.id
     ))
-    message_text = discord_message.content
-    photo_uploader = PhotoMessageUploader(api=bots.vk_bot.api)
-    attachments_list = []
-
-    for full_emoji, is_animated, emoji_id in EMOJI_REGEX.findall(message_text):
-        message_text = message_text.replace(full_emoji, '')
-        emoji_file = await download_emoji(emoji_id=emoji_id, guild_id=discord_message.guild.id)
-        attach = await photo_uploader.upload(file_source=emoji_file)
-        os.remove(emoji_file)
-        attachments_list.append(attach)
-        if len(attachments_list) == 10:
-            break
 
     if nickname:
         author_string = f'{nickname.nickname} ({discord_message.author}):'
@@ -34,13 +24,35 @@ async def get_vk_message(discord_message: discord.Message):
         author_string = f'{discord_message.author}:'
     message_text = f'{author_string}\n{message_text}'
 
-    if attaches := discord_message.attachments:
-        for attach in attaches:
-            if attach.content_type.startswith('image') and len(attachments_list) != 10:
-                image_file = await download_file(url=attach.url, filename=attach.filename)
-                attach = await photo_uploader.upload(file_source=image_file)
-                os.remove(image_file)
-                attachments_list.append(attach)
+    photo_uploader = PhotoMessageUploader(api=bots.vk_bot.api)
+    attachments_list = []
+
+    if attachments := discord_message.attachments:
+        for attachment in attachments:
+            if not attachment.content_type.startswith('image'):
+                continue
+            if len(attachments_list) == 10:
+                break
+
+            image_file = await download_file(
+                url=attachment.url,
+                filename=attachment.filename
+            )
+            vk_attachment = await photo_uploader.upload(file_source=image_file)
+            os.remove(image_file)
+            attachments_list.append(vk_attachment)
+
+    for full_emoji, is_animated, emoji_id in EMOJI_REGEX.findall(message_text):
+        message_text = message_text.replace(full_emoji, '', 1)
+        emoji_file = await download_emoji(
+            emoji_id=emoji_id,
+            guild_id=discord_message.guild.id
+        )
+        vk_attachment = await photo_uploader.upload(file_source=emoji_file)
+        os.remove(emoji_file)
+        attachments_list.append(vk_attachment)
+        if len(attachments_list) == 10:
+            break
 
     attachments = ','.join(attachments_list)
 
