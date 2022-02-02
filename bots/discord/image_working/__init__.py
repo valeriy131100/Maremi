0ada0201bb49ceb515914e9edcf8a464843962ba
Valery Efremov <valeriy131100@gmail.com>
2022-02-02 09:31:54 +0300

    Remove unused import from image_working

--- bots/discord/image_working/__init__.py
+++ bots/discord/image_working/__init__.py
@@ -1,7 +1,6 @@
 import disnake as discord
 from disnake.ext import commands
 
-import freeimagehost
 from bots.discord.utils.galleries import create_gallery
 from bots.discord.utils.webhooks import get_channel_send_webhook
 from bots.discord.utils.wrappers import react_and_delete
